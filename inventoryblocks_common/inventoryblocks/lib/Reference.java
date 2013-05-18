4bef84c530fca370790d0ec2bf6914e9716f5991
mmmfrieddough <mmmfrieddough@gmail.com>
2013-05-18 16:50:41 -0400

    Beginning files added

---
+++ inventoryblocks_common/inventoryblocks/lib/Reference.java
@@ -0,0 +1,8 @@
+package inventoryblocks.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "IB";
+    public static final String MOD_NAME = "Inventory Blocks";
+    public static final String VERSION = "0.0.1";
+}
